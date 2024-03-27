a10ee67d6aae848c1cb479de4e4e686e02abe337
michaelparkernorton <michaelparkernorton.va@gmail.com>
2024-03-27 14:34:30 -0400

    updates

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -10,23 +10,26 @@ const getData = async () => {
   if (response.ok) {
     classData = await response.json();
   }
-	// create a new div element
-	const newDiv = document.createElement("div");
-  const newH2 = document.createElement("h2");
-	const newLink = document.createElement("a")
-  const newImage = document.createElement("img");
 
-  // and give it some content
-	newH2.textContent = classData[0].name
-	newImage.src = classData[0].image
-	newLink.href = classData[0].link
-
-  // add the newly created element and its content into the DOM
-	newDiv.appendChild(newH2)
-	newDiv.appendChild(newLink)
-	newLink.appendChild(newImage);
-  projects.appendChild(newDiv);
-	console.log(classData[0].image);
+	classData.forEach(classProject => {
+		
+		// create a new div element
+		const newDiv = document.createElement("div");
+		const newH2 = document.createElement("h2");
+		const newLink = document.createElement("a")
+		const newImage = document.createElement("img");
+	
+		// and give it some content
+		newH2.textContent = classProject.name
+		newImage.src = classProject.image
+		newLink.href = classProject.link
+	
+		// add the newly created element and its content into the DOM
+		newDiv.appendChild(newH2)
+		newDiv.appendChild(newLink)
+		newLink.appendChild(newImage);
+		projects.appendChild(newDiv);
+	});
 
 };
 
